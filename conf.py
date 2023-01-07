d9a44260b3eae60295febe6adf6b660937093d9a
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-07 16:50:10 +0000

    import data and change preprocessing

---
+++ conf.py
@@ -0,0 +1,62 @@
+import os, random
+import numpy as np
+import torch
+
+
+
+# experiment reproduction
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+
+
+
+root = os.getcwd()
+datapath = os.path.join(root, 'data')
+
+paths = dict(
+
+    root=root, data=datapath,  model=os.path.join(datapath, 'model'),
+
+    liebherr=os.path.join(datapath, 'liebherr'),
+    liebherrvideos=os.path.join(datapath, 'liebherr', 'liebherrvideos'), 
+    liebherrdataset=os.path.join(datapath, 'liebherr', 'liebherrdataset'), 
+        liebherrdatasettrain=os.path.join(datapath, 'liebherr', 'liebherrdataset', 'liebherrdatasettrain'), 
+        liebherrdatasettest=os.path.join(datapath, 'liebherr', 'liebherrdataset', 'liebherrdatasettest'),
+    liebherriframes=os.path.join(datapath, 'liebherr', 'liebherriframes'), 
+        liebherrtrainiframes=os.path.join(datapath, 'liebherr', 'liebherriframes', 'liebherrtrainiframes'),
+        liebherrtestiframes=os.path.join(datapath, 'liebherr', 'liebherriframes', 'liebherrtestiframes'),
+    
+    vision=os.path.join(datapath, 'vision'),
+    visionvideos=os.path.join(datapath, 'vision', 'visionvideos'), 
+    visiondataset=os.path.join(datapath, 'vision', 'visiondataset'), 
+        visiondatasettrain=os.path.join(datapath, 'vision', 'visiondataset', 'visiondatasettrain'), 
+        visiondatasettest=os.path.join(datapath, 'vision', 'visiondataset', 'visiondatasettest'),
+    visioniframes=os.path.join(datapath, 'vision', 'visioniframes'), 
+        visiontrainiframes=os.path.join(datapath, 'vision', 'visioniframes', 'visiontrainiframes'),
+        visiontestiframes=os.path.join(datapath, 'vision', 'visioniframes', 'visiontestiframes'),
+
+
+
+)
+
+
+def creatdir(path):
+    try:
+        os.makedirs(path)
+    except Exception as e:
+        print(e)
+
+def allpaths(dirs: dict):
+    for ke, val in dirs.items():
+        creatdir(val)
+
+def main():
+    print(root)
+    # allpaths(dirs=paths)
+
+
+
+
+if __name__ == '__main__':
+    main()
